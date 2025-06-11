a6e3779ecaaa8e65edb52ee0c49359e3a142f559
ezpieco <ezpie.co@gmail.com>
2025-06-11 18:42:43 +0530

    add a bash script to create local builds

---
+++ tools/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+echo "🧹 Cleaning old builds..."
+
+rm -rf ../builds/
+mkdir -p ../builds/
+
+APPNAME="hooky"
+
+PLATFORMS=(
+  "darwin amd64"
+  "darwin arm64"
+  "linux amd64"
+  "windows amd64"
+)
+
+echo "🚧 Building binaries..."
+
+for platform in "${PLATFORMS[@]}"; do
+  read -r GOOS GOARCH <<< "$platform"
+
+  output_name="$APPNAME-$GOOS-$GOARCH"
+  #[ "$GOOS" == "windows" ] && output_name+=".exe"
+
+  echo "🔧 Building for $GOOS/$GOARCH as $output_name"
+
+  GOOS=$GOOS GOARCH=$GOARCH go build -o "../builds/$output_name" ".."
+
+  echo "✅ $output_name built!"
+done
+
+echo ""
+echo "🎉 all builds completed!"
